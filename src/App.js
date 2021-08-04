376a14c767b552da5330c3037ab94b4b64bb35dd
Alex Semochkin <fennyflop@gmail.com>
2021-08-04 21:38:22 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Suspense} from 'react';
+
+import Model from './components/Three/chest.js';
+import Lights from './components/Three/lights.js';
+import Floor from './floor.js';
+import {Canvas} from "react-three-fiber";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+      <Canvas
+        colorManagement
+        shadowMap
+        camera={{position: [-5, 4, 4], fov: 40}}
+      >
+        <Lights />
+        <Suspense fallback={null}>
+          <Model />
+          <Floor />
+        </Suspense>
+
+      </Canvas>
+      </div>
+    </>
   );
 }
 
